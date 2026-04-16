30655ab359d380cd1ce3ddac676af4fe465a334a
ойойой <153764765+swqolx@users.noreply.github.com>
2026-04-16 09:09:23 -0700

    1

---
+++ hw18.js
@@ -0,0 +1,20 @@
+// const categories = document.querySelectorAll("#categories .item");
+
+// console.log(`In list ${categories.length} category`);
+
+// categories.forEach(category => {
+//   const title = category.querySelector("h2").textContent;
+//   const items = category.querySelectorAll("ul li");
+
+//   console.log(`Name of category: ${title}`);
+//   console.log(`Suma of elements: ${items.length}`);
+// });
+
+const ingredients = [
+ 'Картопля',
+ 'Гриби',
+ 'Часник',
+ 'Помідори',
+ 'Зелень',
+ 'Приправи',
+];
